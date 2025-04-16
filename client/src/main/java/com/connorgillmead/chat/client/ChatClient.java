b975f7feb06a9796687983aa0edfe3f4443f2247
Connor Gill-Mead <gill-mead_connor@sky.com>
2025-04-16 22:10:46 +0100

    intialised multi-module scaffolding

---
+++ client/src/main/java/com/connorgillmead/chat/client/ChatClient.java
@@ -0,0 +1,13 @@
+package com.connorgillmead.chat.client;
+
+/** */
+public final class ChatClient {
+
+    private ChatClient() {
+        // Prevent instantiation
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Client stub OK");
+    }
+}
---
+++ server/src/main/java/com/connorgillmead/chat/server/ChatServer.java
@@ -0,0 +1,13 @@
+package com.connorgillmead.chat.server;
+
+/** */
+public final class ChatServer {
+
+    private ChatServer() {
+        // Prevent instantiation
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Server stub OK");
+    }
+}
